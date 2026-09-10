66f766d03be87a319ec69f75858b3195648ac1cc
Marli0203 <2605748@sit.singaporetech.edu.sg>
2026-09-10 21:12:41 +0800

    Lab2: Added Req 5 to 8

--- auditor.py
+++ auditor.py
@@ -2,6 +2,7 @@
 
 #Requirement 1
 inventory = 0
+failedEntriesCounter = 0
 
 #Requirement 2
 while True:
@@ -9,12 +10,26 @@ while True:
     stock = input("Enter a stock quantity: ").lower().strip()
 
     if stock == "quit":
+        failedEntriesCounter += 1
         break;
 
-    #Requirement 4
-    if stock.isdigit() == False:
+    #Requirement 4 and Requirement 5
+    if stock.isdigit() == False or int(stock) < 0:
         print("Error: '" + stock + "' is not a valid numerical input!")
+        failedEntriesCounter += 1
         continue
 
     #Requirement 3
     stock = int(stock)
+
+    #Requirement 6
+    inventory += stock
+
+    #Requirement 7
+    if inventory > 500:
+        print("Alert: Total inventory has exceeded 500 units!")
+        failedEntriesCounter += 1
+        break;
+
+#Requirement 8
+print("\nTotal Units Processed:", inventory, "\nNumber of Failed Entries:", failedEntriesCounter)
