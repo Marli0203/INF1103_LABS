161cd2db3dbf53c2552a4d91301beb2be3692cec
Marli0203 <2605748@sit.singaporetech.edu.sg>
2026-09-17 22:54:41 +0800

    Lab3: Improved get_valid_input() functionality

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,16 +1,15 @@
 #This is the code for INF 1103 Lab 2
 
 # Defining Functions
-def get_valid_input(failedEntriesCounter):
+def get_valid_input():
     # Prompt User for Input
     user_input = input("Enter a stock quantity: ").lower().strip()
 
     # Check For Invalid Input
     if user_input != "quit" and (user_input.isdigit() == False or int(user_input) <= 0):
         print("Error: '" + user_input + "' is not a valid input!") # Print Error Message
-        failedEntriesCounter += 1
-        user_input, failedEntriesCounter = get_valid_input(failedEntriesCounter); #Re-prompt
-    return user_input, failedEntriesCounter
+        user_input = "invalid"
+    return user_input
 
 def process_delivery(current_total: int, new_value: int):
     # Add New Value To Current Total
@@ -30,16 +29,21 @@ failedEntriesCounter = 0
 # Lab2: Requirement 2
 while True:
     # Lab3: Requirement 1
-    user_input, failedEntriesCounter = get_valid_input(failedEntriesCounter)
+    user_input = get_valid_input()
+
     if user_input == "quit":
         break;
+    
+    if user_input == "invalid":
+        failedEntriesCounter += 1
+        continue
 
     # Lab3: Requirement 2
     if process_delivery(inventory, int(user_input)) > 500:
         print("Alert: Total inventory has exceeded 500 units!")
         failedEntriesCounter += 1
         break;
-
+    
     inventory = process_delivery(inventory, int(user_input))
 
 # Lab2: Requirement 8
