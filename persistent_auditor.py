db810f386b9dbd20bb6aca0091553c1e88b64f30
Marli0203 <2605748@sit.singaporetech.edu.sg>
2026-09-24 17:02:12 +0800

    Lab 4: Created load_inventory function

---
+++ persistent_auditor.py
@@ -0,0 +1,59 @@
+# Defining Functions
+def get_valid_input():
+    # Prompt User for Input
+    user_input = input("Enter a stock quantity: ").lower().strip()
+
+    # Check For Invalid Input
+    if user_input != "quit" and (user_input.isdigit() == False or int(user_input) <= 0):
+        print("Error: '" + user_input + "' is not a valid input!") # Print Error Message
+        user_input = "invalid"
+    return user_input
+
+def process_delivery(current_total: int, new_value: int):
+    # Add New Value To Current Total
+    current_total += new_value
+    return current_total
+
+def calculate_tax(amount: float):
+    return amount * 0.1
+
+def generate_report(total_units: int, failed_attempts: int):
+    print("\nTotal Units Processed:", total_units, "\nTotal Tax:", calculate_tax(float(total_units)), "\nNumber of Failed Entries:", failed_attempts)
+    return
+
+def load_inventory():
+    # Create file if it does not exist
+    file = open(".\\INF1103-Labs\\inventory.txt", "a")
+    file.close()
+
+    # Load transaction history
+    file = open(".\\INF1103-Labs\\inventory.txt", "r")
+    transaction_history = file.readlines()
+    file.close()
+
+    return transaction_history
+
+# Variable Initialization
+inventory = 0
+failedEntriesCounter = 0
+
+transaction_history = load_inventory()
+
+while True:
+    user_input = get_valid_input()
+
+    if user_input == "quit":
+        break;
+    
+    if user_input == "invalid":
+        failedEntriesCounter += 1
+        continue
+
+    if process_delivery(inventory, int(user_input)) > 500:
+        print("Alert: Total inventory has exceeded 500 units!")
+        failedEntriesCounter += 1
+        break;
+    
+    inventory = process_delivery(inventory, int(user_input))
+
+generate_report(inventory, failedEntriesCounter)
